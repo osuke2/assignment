244b3f36fcda5ec8f27e7c38f1754cb5cfa7e329
平野　慧 <tantan@Scherzo.local>
2023-06-01 22:17:58 +0900

    日本とトルコのデータ

--- project1.m
+++ project1.m
@@ -1,1 +1,11 @@
+clc
+clear all
+close all
+
+f=fred 
+startdate='01/01/1998';
+enddate='07/01/2022';
+
+realGDPforJ=fetch(f,'JPNRGDPEXP',startdate,enddate);
+realGDPforT=fetch(f,'NGDPRSAXDCTRQ',startdate,enddate);
 
