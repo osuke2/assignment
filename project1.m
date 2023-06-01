a32a543cd4899cbc8612df02d49fc31448811fc6
平野　慧 <tantan@Scherzo.local>
2023-06-01 23:45:21 +0900

    HPフィルター関数を一番下に移動&end追加

--- project1.m
+++ project1.m
@@ -9,22 +9,6 @@ enddate='07/01/2022';
 realGDPforJ=fetch(f,'JPNRGDPEXP',startdate,enddate);
 realGDPforT=fetch(f,'NGDPRSAXDCTRQ',startdate,enddate);
 
-function [ytilde,tauGDP]=qmacro_hpfilter(y,lam)
-T=size(y,1);
-A=zeros(T,T);
-A(1,1)=lam+1; A(1,2)= -2*lam; A(1,3)= lam;
-A(2,1)= -2*lam; A(2,2)= 5*lam+1; A(2,3)= -4*lam; A(2,4)= lam;
-A(T-1,T)= -2*lam; A(T-1,T-1)= 5*lam+1; A(T-1,T-2)= -4*lam; A(T-1,T-3)= lam;
-A(T,T)= lam+1; A(T,T-1)= -2*lam; A(T,T-2)= lam;
-
-for i=3:T-2
-    A(i,i-2) = lam; A(i,i-1) = -4*lam; A(i,i) = 6*lam+1;
-    A(i,i+1) = -4*lam; A(i,i+2) = lam;
-end
-
-tauGDP = A\y;
-ytilde=y-tauGDP;
-
 year1 = realGDPforJ.Data(:,1);
 y1 = realGDPforJ.Data(:,2);
 year2 = realGDPforT.Data(:,1);
@@ -54,6 +38,25 @@ hold off;
 text(729846, 0.09, sprintf('corrcoef = %f', corr_JT)); 
 text(729846, 0.07, sprintf('std_J = %f', std_J));
 text(729846, 0.05, sprintf('std_T = %f', std_T));
+
+function [ytilde,tauGDP]=qmacro_hpfilter(y,lam)
+T=size(y,1);
+A=zeros(T,T);
+A(1,1)=lam+1; A(1,2)= -2*lam; A(1,3)= lam;
+A(2,1)= -2*lam; A(2,2)= 5*lam+1; A(2,3)= -4*lam; A(2,4)= lam;
+A(T-1,T)= -2*lam; A(T-1,T-1)= 5*lam+1; A(T-1,T-2)= -4*lam; A(T-1,T-3)= lam;
+A(T,T)= lam+1; A(T,T-1)= -2*lam; A(T,T-2)= lam;
+
+for i=3:T-2
+    A(i,i-2) = lam; A(i,i-1) = -4*lam; A(i,i) = 6*lam+1;
+    A(i,i+1) = -4*lam; A(i,i+2) = lam;
+end
+
+tauGDP = A\y;
+ytilde=y-tauGDP;
+end
+
+
 
 
 
